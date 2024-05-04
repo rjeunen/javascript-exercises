d3689a0142be257ae01ed8c6bdfdbbca9e529648
Robby Jeunen <robbyjeunen@gmail.com>
2024-05-04 19:59:57 +0200

    resolved execercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,23 @@
-const palindromes = function () {
+const palindromes = function (palindrome) {
+    palindrome = palindrome.toLowerCase();
+    palindrome = palindrome.replaceAll(" ", "");
+    palindrome = palindrome.replaceAll(",", "");
+    palindrome = palindrome.replaceAll(".", "");
+    palindrome = palindrome.replaceAll("!", "");
+    let reversedPalindrome = palindrome.split("").reverse().join("");
+    reversedPalindrome = reversedPalindrome.toLowerCase();
+    reversedPalindrome = reversedPalindrome.replaceAll(" ", "");
+    reversedPalindrome = reversedPalindrome.replaceAll(",", "");
+    reversedPalindrome = reversedPalindrome.replaceAll(".", "");
+    reversedPalindrome = reversedPalindrome.replaceAll("!", "");
+    if(reversedPalindrome === palindrome){
+        return true;
+    } else{
+        return false;
+    }
 
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
